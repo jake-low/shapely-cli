4a8ef613fa7664efac4127e98c4cf6211ef9e72a
Jake Low <hello@jakelow.com>
2025-01-31 12:12:41 -0800

    Add some failing tests for compact (not pretty-printed) GeoJSON inputs

--- tests/run.sh
+++ tests/run.sh
@@ -3,3 +3,8 @@
 # run suite twice, once for GeoJSON and once for NDJSON
 env GREAT_LAKES_INPUT_FILE="tests/great-lakes.geojson" bats $@ tests/suite.sh
 env GREAT_LAKES_INPUT_FILE="tests/great-lakes.ndjson" bats $@ tests/suite.sh
+
+# run one more time, on a compact (not pretty printed) version of the GeoJSON
+jq -c < tests/great-lakes.geojson > tests/great-lakes.compact.geojson
+env GREAT_LAKES_INPUT_FILE="tests/great-lakes.compact.geojson" bats $@ tests/suite.sh
+rm tests/great-lakes.compact.geojson
